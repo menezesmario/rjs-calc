905e92bece3bea0f98f54812287d2343481ffee0
Mário Menezes <mario082@gmail.com>
2021-03-24 09:59:52 -0300

    display

---
+++ src/components/display.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react' ;
+
+
+
+
+class Display extends Components {
+    render() {
+        return(
+        <div className={"display borderBlack"}>
+            {this.props.value}
+        </div>
+        )
+    }
+}
+
+export default Display;
+
