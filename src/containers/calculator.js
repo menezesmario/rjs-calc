f80115e7b76ac5e5a093c4b796bae304ee4b278e
Mário Menezes <mario082@gmail.com>
2021-03-24 23:01:21 -0300

    putValue

--- src/containers/calculator.js
+++ src/containers/calculator.js
@@ -4,27 +4,41 @@ import Display from '../components/display';
 
 class Calculator extends Component{
     initialState = {firstValue: 0, secondValue:0}
+
+    constructor(props) {
+        super(props);
+        this.state = this.initialState;
+    }
+
+    putValue = (value) => {
+        const lastValue = this.state.firstValue;
+        this.setState({firstValue:(lastValue*10)+value})
+    }
+
+    getValue = () => {
+        return this.state.firstValue;
+    }
     render() {
         return (
             <div className={'calculator'}>
                 <div>
-                    <Display value={'123456'}/>
+                    <Display value={this.getValue()}/>
                 </div>
                 <div className={'buttons'}>
-                    <Button display="1"/>
-                    <Button display="2"/>
-                    <Button display="3"/>
-                    <Button display="4"/>
-                    <Button display="5"/>
-                    <Button display="6"/>
-                    <Button display="7"/>
-                    <Button display="8"/>
-                    <Button display="9"/>
-                    <Button display="0"/>
-                    <Button display="+"/>
-                    <Button display="-"/>
-                    <Button display="="/>
-                    <Button display="C"/>
+                    <Button display={"1"} onClick={()=>this.putValue(1)}/>
+                    <Button display={"2"} onClick={()=>this.putValue(2)}/>
+                    <Button display={"3"} onClick={()=>this.putValue(3)}/>
+                    <Button display={"4"} onClick={()=>this.putValue(4)}/>
+                    <Button display={"5"} onClick={()=>this.putValue(5)}/>
+                    <Button display={"6"} onClick={()=>this.putValue(6)}/>
+                    <Button display={"7"} onClick={()=>this.putValue(7)}/>
+                    <Button display={"8"} onClick={()=>this.putValue(8)}/>
+                    <Button display={"9"} onClick={()=>this.putValue(9)}/>
+                    <Button display={"0"} onClick={()=>this.putValue(0)}/>
+                    <Button display={"+"} />
+                    <Button display={"-"} />
+                    <Button display={"="} />
+                    <Button display={"C"} />
                 </div>
                 <div className={'footer'}>
                     reactCalc
