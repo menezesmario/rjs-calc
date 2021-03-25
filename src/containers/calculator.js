2fb1a30bd54fe0d0608785a8b2e199956ae96aa4
Mário Menezes <mario082@gmail.com>
2021-03-24 22:28:02 -0300

    css base

--- src/containers/calculator.js
+++ src/containers/calculator.js
@@ -3,24 +3,32 @@ import { Button } from '../components/button';
 import Display from '../components/display';
 
 class Calculator extends Component{
+    initialState = {firstValue: 0, secondValue:0}
     render() {
         return (
             <div className={'calculator'}>
-                <Display value={'123456'}/>
-                <Button display="1"/>
-                <Button display="2"/>
-                <Button display="3"/>
-                <Button display="4"/>
-                <Button display="5"/>
-                <Button display="6"/>
-                <Button display="7"/>
-                <Button display="8"/>
-                <Button display="9"/>
-                <Button display="0"/>
-                <Button display="="/>
-                <Button display="-"/>
-                <Button display="="/>
-                <Button display="C"/>
+                <div>
+                    <Display value={'123456'}/>
+                </div>
+                <div className={'buttons'}>
+                    <Button display="1"/>
+                    <Button display="2"/>
+                    <Button display="3"/>
+                    <Button display="4"/>
+                    <Button display="5"/>
+                    <Button display="6"/>
+                    <Button display="7"/>
+                    <Button display="8"/>
+                    <Button display="9"/>
+                    <Button display="0"/>
+                    <Button display="+"/>
+                    <Button display="-"/>
+                    <Button display="="/>
+                    <Button display="C"/>
+                </div>
+                <div className={'footer'}>
+                    reactCalc
+                </div>
             </div>
         )
     }
